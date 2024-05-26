2c6d4f4fef7e66174bfdb89148f8aadfb83d1482
KarinaSmu <Smumrrr@mail.ru>
2024-05-26 10:04:04 +0300

    admin settings added

--- homework/shopapp/admin.py
+++ homework/shopapp/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Client, Product, Order, OrderItem
 
-# Register your models here.
+
+@admin.action(description="Сбросить количество в ноль")
+def reset_quantity(modeladmin, request, queryset):
+    queryset.update(quantity=0)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'price', 'quantity', 'date_added']
+    list_filter = ['price', 'quantity']
+    search_fields = ['name']
+    search_help_text = 'Поиск по названию'
+    actions = [reset_quantity]
+    readonly_fields = ['date_added']
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'address', 'registration_date')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'client', 'total_amount', 'date_placed')
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'quantity')
